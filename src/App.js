3f4e9156df3fe62c62cd7f1a9f7eae89e8b4de61
NambiMR <kuttypaiyan618016@gmail.com>
2024-09-05 11:06:27 -0700

    update 1

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <Portfolio />
       <Skills />
       <Contact />
-
+  
       <SocialLinks />
     </div>
   );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,8 +1,50 @@
 import React from 'react'
-
+import { FaLinkedin } from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
+import { IoMail } from "react-icons/io5";
+import { ImProfile } from "react-icons/im";
 const Contact = () => {
+    const links=[
+        {
+            id:1,
+            child:(
+                <>
+                    <ImProfile  size={30}/>
+                </>
+            ),
+            href:"/resume.pdf",
+            download:true,
+        },
+        {
+            id:2,
+            child:(
+                <>
+                    <FaLinkedin size={30}/>
+                </>
+            ),
+            href:"https://www.linkedin.com/in/nambi25 ",
+        },
+        {
+            id:3,
+            child:(
+                <>
+                    <FaSquareGithub size={30}/>
+                </>
+            ),
+            href:"https://github.com/NambiMR",
+        },
+        {
+            id:4,
+            child:(
+                <>
+                    <IoMail size={30}/>
+                </>
+            ),
+            href:"mailto:nambimr2004@gmail.com",
+        },
+    ]
   return (
-    <div name="contact" className='w-full h-screen bg-gradient-to-b from-black to bg-gray-800 p-4 text-white'>
+    <div name="contact" className='w-full h-auto bg-gradient-to-b from-black to bg-gray-800 p-4 text-white'>
         <div className='flex flex-col p-4 justify-center max-w-screen-lg mx-auto h-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-gray-300'>Contact</p>
@@ -18,6 +60,24 @@ const Contact = () => {
                     </textarea>
                     <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's Talk</button>
                 </form>
+            </div>
+            <div className='md:hidden'>
+            <p className='p-4'>Check out my resume here</p>
+                <ul className='flex justify-center gap-2'>
+                {links.map(({id, child, href, download})=>(
+                        <li key={id} className='flex justify-center items-center w-40  px-4  hover:scale-110 hover:rounded-md duration-300'>
+                        <a href={href} 
+                        className='flex justify-center items-center w-full text-white '
+                        download={download}
+                        target='_blank'
+                        rel="noreferrer"
+                        >
+                        {child}
+                        
+                        </a>
+                    </li>
+                    ))}
+                </ul>
             </div>
         </div>
     </div>
